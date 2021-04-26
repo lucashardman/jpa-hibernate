402d30517c6e82c27fdb58be6a652ee6b0ba66f1
Lucas Hardman <lucas.hardman92@gmail.com>
2021-04-26 13:01:31 -0300

    model categoria

---
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -0,0 +1,36 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categorias")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -4,24 +4,26 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("iPhone");
-		celular.setDescricao("Muito legal");
-		celular.setPreco(new BigDecimal("800"));
+		Categoria celulares = new Categoria("celulares");
+		Produto celular = new Produto("iPhone","Muito legal",new BigDecimal("800"), celulares );
 		
 		EntityManager em  = JPAUtil.getEntityManager();
 		
-		ProdutoDAO dao = new ProdutoDAO(em);
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();	
 	}
